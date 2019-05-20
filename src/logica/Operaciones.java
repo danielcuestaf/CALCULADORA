53700e318033b40873ce912cd83d00868aff8d49
danielcuestaf <43210010+danielcuestaf@users.noreply.github.com>
2019-05-20 11:40:06 -0500

    Add files via upload

---
+++ src/logica/Operaciones.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Operaciones {
+    
+    public int suma(int x, int y){
+        
+        int suma = x + y;
+        return suma;
+    }
+    
+}
